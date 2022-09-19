67931ac7e0f146b3c8411c0a2e6e17e6e638fd04
todd-buck <btbuckjr@gmail.com>
2022-09-19 12:22:55 -0500

    Created Additional Test Case (Empty String read successfully)

--- ScannerTests/ScannerTests.kt
+++ ScannerTests/ScannerTests.kt
@@ -6,12 +6,24 @@ import kotlin.collections.ArrayList
 
 class ScannerTests {
     @Test
-    fun firstTest() {
+    fun predefinedOutput_String_SUCCESS() {
         val input = Scanner("4 * 2 + 6")
         val test: MutableList<String> = ArrayList()
         val target: MutableList<String> = mutableListOf("4","*","2","+","6")
 
         for(x in input.tokens())
+            test.add(x.toString())
+
+        assert(test == target)
+    }
+
+    @Test
+    fun predefinedOutput_NULL_SUCCESS() {
+        val input = Scanner("")
+        val test: MutableList<String> = ArrayList()
+        val target: MutableList<String> = mutableListOf()
+
+        for (x in input.tokens())
             test.add(x.toString())
 
         assert(test == target)
